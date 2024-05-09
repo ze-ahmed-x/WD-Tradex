d0e1d081f60560948e6ec55463853bec4133e36e
Zeshan Ahmed <ze.ahmed.x@protonmail.com>
2024-05-09 14:57:13 +0500

    Upload thing with default configuration

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,12 +1,27 @@
+'use client'
+import { UploadButton } from '@/lib/uploadthing'
 import { underConstructionImage } from '@/public'
 import Image from 'next/image'
 import React from 'react'
+
 
 const page = () => {
   return (
     <section className='custom_container mt-10 min-h-screen'>
         <div className='flex items-start justify-center'>
-            <Image src = { underConstructionImage.src} alt='underConstruction' height={729} width={972} />
+            {/* <Image src = { underConstructionImage.src} alt='underConstruction' height={729} width={972} /> */}
+            <UploadButton
+        endpoint="imageUploader"
+        onClientUploadComplete={(res) => {
+          // Do something with the response
+          console.log("Files: ", res);
+          alert("Upload Completed");
+        }}
+        onUploadError={(error: Error) => {
+          // Do something with the error.
+          alert(`ERROR! ${error.message}`);
+        }}
+      />
         </div>
     </section>
   )
